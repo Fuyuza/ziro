b257fe8b37af81115b20e0ea611dc9938e8157ad
Fuser <109461244+Fuyuza@users.noreply.github.com>
2022-12-31 13:23:12 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name="ziro",
+  version="0.1",
+  long_description=open("README.md").read(),
+  long_description_content_type='text/markdown',
+  packages=["ziro"],
+  keywords="hide_message message zero-width-character",
+)
